d40fc3abb67edc6f054ada06801128a80a9287c5
Abshar777 <absharameen625@gmail.com>
2025-04-17 13:52:15 +0530

    fix: image error in depoly

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
+
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // This will ignore all ESLint errors during build
