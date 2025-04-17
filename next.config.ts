81e0c6bd8d6b0dba78f741e78b8ce11cdead3b20
Abshar777 <absharameen625@gmail.com>
2025-04-17 13:48:36 +0530

    feat: unoptimized adedd in next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // This will ignore all ESLint errors during build
--- src/constants/authLayout.tsx
+++ src/constants/authLayout.tsx
@@ -1,27 +1,27 @@
-import loginFormImage from "@/../public/loginFormImage.f7f800ccb930349b460a.png"
-import otpFormImage from "@/../public/otp.png"
-import forgotPasswordImage from "@/../public/forgotPassword.png"
-import conformPasswordImage from "@/../public/conformPassword.png"
+import loginFormImage from "@/../public/loginFormImage.f7f800ccb930349b460a.png";
+import otpFormImage from "@/../public/otp.png";
+import forgotPasswordImage from "@/../public/forgotPassword.png";
+import conformPasswordImage from "@/../public/conformPassword.png";
 
-
-export const quateImages = [{
+export const quateImages = [
+  {
     path: "/auth/login",
-    image: loginFormImage
-
-}, {
+    image: loginFormImage,
+  },
+  {
     path: "/auth/register",
-    image: loginFormImage
-}, {
+    image: loginFormImage,
+  },
+  {
     path: "/auth/otp",
-    image: otpFormImage
-}, {
+    image: otpFormImage,
+  },
+  {
     path: "/auth/forgot-password",
-    image: forgotPasswordImage
-}, {
+    image: forgotPasswordImage,
+  },
+  {
     path: "/auth/conform-password",
-    image: conformPasswordImage
-}
-]
-
-
-
+    image: conformPasswordImage,
+  },
+];
